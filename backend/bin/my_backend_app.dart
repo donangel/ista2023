1d32cffc549cab36290a811deb944c50add84351
Doncho Angelov <public@doncho.net>
2023-09-30 13:17:56 +0300

    the basic backend generated.

---
+++ backend/bin/my_backend_app.dart
@@ -0,0 +1,5 @@
+import 'package:my_backend_app/my_backend_app.dart' as my_backend_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_backend_app.calculate()}!');
+}
---
+++ backend/test/my_backend_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_backend_app/my_backend_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
