2686ae2f11d5250e0355ae6f0de552329b8d51bd
Doncho Angelov <public@doncho.net>
2023-09-30 13:20:52 +0300

    It seems we're ready with our initial Docker build?

--- backend/bin/my_backend_app.dart
+++ backend/bin/my_backend_app.dart
@@ -1,5 +1,16 @@
-import 'package:my_backend_app/my_backend_app.dart' as my_backend_app;
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as io;
 
-void main(List<String> arguments) {
-  print('Hello world: ${my_backend_app.calculate()}!');
+void main() {
+  var handler = const shelf.Pipeline()
+      .addMiddleware(shelf.logRequests())
+      .addHandler(_echoRequest);
+
+  io.serve(handler, 'localhost', 8080).then((server) {
+    print('Serving at http://${server.address.host}:${server.port}');
+  });
+}
+
+shelf.Response _echoRequest(shelf.Request request) {
+  return shelf.Response.ok('Hello, Dart Backend!');
 }
